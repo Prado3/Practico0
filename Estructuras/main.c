f74712e3e6eaf3ec53c95c37c33a7f4550f08cb2
Lucas Prado <83882214+Prado3@users.noreply.github.com>
2023-05-23 20:26:36 -0300

    Add files via upload

--- Estructuras/main.c
+++ Estructuras/main.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <conio.h>
 #include "legajo.h"
+#include <string.h>
 #define DIM_LEGAJOS 1000
 #define ESC 27
 
 int cargarArregloLegajo(stLegajo legajos[], int dim);
 void muestrLegajo(stLegajo legajos[],int v);
+void ordenarLegajoMatricula(stLegajo legajos[],int v);
 
 int main()
 {
@@ -14,6 +16,8 @@ int main()
     int vLegajo=0;
 
     vLegajo=cargarArregloLegajo(legajos,DIM_LEGAJOS);
+    muestrLegajo(legajos,vLegajo);
+    ordenarLegajoMatricula(legajos,vLegajo);
     muestrLegajo(legajos,vLegajo);
 
     return 0;
@@ -36,5 +40,31 @@ int cargarArregloLegajo(stLegajo legajos[], int dim){
 void muestrLegajo(stLegajo legajos[],int v){
     for(int i =0;i<v;i++){
         muestraUnLegajo(legajos[i]);
+    }
+}
+
+void ordenarLegajoMatricula(stLegajo legajos[],int v){
+    int posMenor=0;
+    int j=0;
+    int z=0;
+
+    for(int i =0; i<v;i++){
+        for(j=0;j<v;j++){
+            if(legajos[j].matricula<legajos[posMenor].matricula){
+                posMenor=j;
+            }
+        }
+        for(z=v;z>=0;z--){
+            legajos[z+1].matricula=legajos[z].matricula;
+        }
+        legajos[i].matricula=legajos[posMenor].matricula;
+        for(z=v;z>=0;z--){
+            strcpy(legajos[z+1].nombres, legajos[z].nombres);
+        }
+        strcpy(legajos[i].nombres, legajos[posMenor].nombres);
+        for(z=v;z>=0;z--){
+            strcpy(legajos[z+1].genero, legajos[z].genero);
+        }
+        strcpy(legajos[i].genero, legajos[posMenor].genero);
     }
 }
